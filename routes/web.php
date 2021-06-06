766daa9404aaef5abfcfd2611c33ca0b49b4ddf0
Nugroho Adi Pratomo <nugrohoadi.pratomo24@gmail.com>
2021-06-06 11:46:41 +0700

    setting heroku

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'DashboardController@index')->name('dashboard');
 
-Auth::routes(['register' => false]);
+Auth::routes();
 
 Route::get('products/{id}/gallery', 'ProductController@gallery')
     ->name('products.gallery');
